9f142e874d89ee997a288b9cb82c218f1b36b8a0
epeljto1 <epeljto1@etf.unsa.ba>
2025-03-27 10:48:27 +0100

    Added Auth MS

---
+++ Auth/src/main/java/com/example/auth/model/User.java
@@ -0,0 +1,81 @@
+package com.example.auth.model;
+
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.util.UUID;
+
+@Entity
+@Table(name="Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    @ManyToOne
+    @JoinColumn(name="role_id",nullable = false)
+    private Role role;
+
+    // @GeneratedValue(strategy = GenerationType.UUID)
+    // private UUID uuid;
+
+    public User(){}
+    public User(String firstName, String lastName, String username, String password, Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
