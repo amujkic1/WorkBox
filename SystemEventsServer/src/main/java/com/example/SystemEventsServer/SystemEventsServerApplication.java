276040bb9810abba8efadc24b7f5af28e6e98caa
epeljto1 <epeljto1@etf.unsa.ba>
2025-05-26 21:47:56 +0200

    Added System Events Server

---
+++ SystemEventsServer/src/main/java/com/example/SystemEventsServer/SystemEventsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.SystemEventsServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemEventsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemEventsServerApplication.class, args);
+	}
+
+}
