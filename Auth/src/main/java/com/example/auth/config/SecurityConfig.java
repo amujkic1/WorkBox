fbcd9b6e1da3800e61ebb05dd56998749e6dbd9d
amujkic1 <amujkic1@etf.unsa.ba>
2025-05-29 09:24:27 +0200

    added auth ms to api gateway

--- Auth/src/main/java/com/example/auth/config/SecurityConfig.java
+++ Auth/src/main/java/com/example/auth/config/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        //.requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         //.requestMatchers("/api/v1/users/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated()
--- Auth/src/main/java/com/example/auth/controller/AuthController.java
+++ Auth/src/main/java/com/example/auth/controller/AuthController.java
@@ -6,7 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/auth")
+//@RequestMapping("api/v1/auth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
@@ -21,5 +22,4 @@ public class AuthController {
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
         return ResponseEntity.ok(service.authenticate(request));
     }
-
 }
