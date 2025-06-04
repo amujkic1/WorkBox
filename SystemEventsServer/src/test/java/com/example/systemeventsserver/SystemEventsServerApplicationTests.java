dded6740b7d935481e63ac8e75a83dbec053bd27
epeljto1 <epeljto1@etf.unsa.ba>
2025-06-04 12:35:42 +0200

    Fixed SystemEventsServer

--- SystemEventsServer/src/test/java/com/example/systemeventsserver/SystemEventsServerApplicationTests.java
+++ SystemEventsServer/src/test/java/com/example/systemeventsserver/SystemEventsServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.systemeventsserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "grpc.enabled=false")
 class SystemEventsServerApplicationTests {
 
 	@Test
